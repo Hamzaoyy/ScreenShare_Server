4725797658c6b7f284b172b4432bae69b1e7617d
Humble_HUb <maharzahidemail.com@gmail.com>
2026-05-23 10:51:41 -0700

    upgrade to WebRTC

--- index.js
+++ index.js
@@ -14,33 +14,43 @@ const io = new Server(server, {
 const rooms = {}
 
 io.on('connection', (socket) => {
-  console.log('Device connected:', socket.id)
+  console.log('Connected:', socket.id)
 
   socket.on('create-room', (pin) => {
     rooms[pin] = socket.id
     socket.join(pin)
-    console.log('Room created with PIN:', pin)
+    console.log('Room created:', pin)
   })
 
   socket.on('join-room', (pin) => {
     if (rooms[pin]) {
       socket.join(pin)
-      io.to(pin).emit('viewer-joined')
-      console.log('Viewer joined PIN:', pin)
+      io.to(rooms[pin]).emit('viewer-joined', socket.id)
+      socket.emit('room-joined', rooms[pin])
     } else {
       socket.emit('invalid-pin')
     }
   })
 
-  socket.on('screen-data', (data) => {
-    socket.to(data.pin).emit('screen-data', data.image)
+  socket.on('offer', (data) => {
+    io.to(data.target).emit('offer', { sdp: data.sdp, from: socket.id })
+  })
+
+  socket.on('answer', (data) => {
+    io.to(data.target).emit('answer', { sdp: data.sdp, from: socket.id })
+  })
+
+  socket.on('ice-candidate', (data) => {
+    io.to(data.target).emit('ice-candidate', { candidate: data.candidate, from: socket.id })
   })
 
   socket.on('disconnect', () => {
-    console.log('Device disconnected:', socket.id)
+    for (const pin in rooms) {
+      if (rooms[pin] === socket.id) delete rooms[pin]
+    }
   })
 })
 
-server.listen(3000, () => {
-  console.log('Server running on port 3000')
+server.listen(process.env.PORT || 3000, () => {
+  console.log('Server running!')
 })
